8f16e42afa3fd89c83c4be05e50c5e056d95c314
Fernando Rodriguez <fernando@cloudcs.com>
2015-06-24 23:01:25 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+/// <reference path="typings/angularjs/angular.d.ts"/>
+(function() {
+  
+  var app = angular
+  .module("githubViewer", ["ngRoute"]);
+  
+  app.config(function($routeProvider){
+    
+    $routeProvider
+      .when("/main", 
+      {
+        templateUrl: "main.html",
+        controller: "MainController"
+      })
+      .when("/user/:username",
+      {
+        templateUrl: "user.html",
+        controller: "UserController"
+      })
+      .when("/repo/:username/:reponame",
+      {
+        templateUrl: "repo.html",
+        controller: "RepoController"
+      })
+      .otherwise({redirectTo:"/main"});
+    
+  });
+  
+  
+}());
